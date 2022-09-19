f9d8597cc2460fddf45dad9fae02b903b5a28436
balsa-asanovic <balsaasanovic95@gmail.com>
2022-09-19 23:48:05 +0200

    Added CSS for users list

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,18 +1,19 @@
 const Users = ({ users, myUsername, getChatFriend }) => {
     return (
-        <div>
-            <h2><span>{myUsername}'s</span> Chats</h2>
+        <div className="flex flex-col border-r-2 w-1/4 bg-white rounded-2xl">
+            <h2 className="py-2 pb-2 pl-2 text-lg border-b border-gray-500 text-gray-600"><span className="font-bold">{myUsername}'s</span> Chats</h2>
             <ul>
                 {users && users.filter((user) => user.username !== myUsername).map((user) => {
                     return (
                     <li key={user.id}>
-                    <button onClick={() => getChatFriend(user.id, user.username)}>
-                        <div>
-                            <div>
-                                <span>{user.username}</span>
-                                <span></span>
+                    <button onClick={() => getChatFriend(user.id, user.username)}
+                            className="flex w-full items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
+                        <div className="w-full pb-2">
+                            <div className="flex justify-between">
+                                <span className="block ml-2 font-semibold text-gray-600">{user.username}</span>
+                                <span className="block ml-2 text-sm"></span>
                             </div>
-                            <span></span>
+                            <span className="flex ml-2 text-sm text-gray-600"></span>
                         </div>
                     </button>
                 </li>)
