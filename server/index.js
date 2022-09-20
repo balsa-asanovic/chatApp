b9d6dc916b22bef2519ccebeb905231a8e86edb5
Balsa Asanovic <balsaasanovic95@gmail.com>
2022-09-20 01:31:26 +0000

    Update 5 files

--- server/index.js
+++ server/index.js
@@ -10,50 +10,49 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: ["http://localhost:3000", "https://ol8fxn-3000.preview.csb.app"],
+    methods: ["GET", "POST"],
+  },
 });
 
 let users = [];
 
 io.on("connection", (e) => {
+  // server-side listener for join_chat emit, when user get's though the initial form
+  // returns false if the same username is already connected
+  // otherwise returns true for and returns users ID
+  e.on("join_chat", (data) => {
+    if (users.find((user) => user.username === data)) {
+      e.emit("join_status", false);
+    } else {
+      users.push({ id: e.id, username: data });
+      io.sockets.emit("users_list", users);
+      e.emit("join_status", true);
+      e.emit("id", e.id);
+    }
+  });
 
-    // server-side listener for join_chat emit, when user get's though the initial form
-    // returns false if the same username is already connected
-    // otherwise returns true for and returns users ID
-    e.on("join_chat", (data) => {
-        if (users.find((user) => user.username === data)) {
-            e.emit("join_status", false);
-        } else {
-            users.push({id: e.id, username: data});
-            io.sockets.emit("users_list", users);
-            e.emit("join_status", true);
-            e.emit("id", e.id);
-        }
-    });
+  // listener for message send, which then emits message to destination id
+  e.on("send_message", (data) => {
+    io.to(data.id).emit("receive_message", data);
+  });
 
-    // listener for message send, which then emits message to destination id
-    e.on("send_message", (data) => {
-        io.to(data.id).emit("receive_message", data);
-    });
+  e.on("username_change", (data) => {
+    users = [...users.filter((user) => user.id !== data.id), data];
+    io.sockets.emit("users_list", users);
+  });
 
-    e.on("username_change", (data) => {
-        users = [...users.filter((user) => user.id !== data.id), data];
-        io.sockets.emit("users_list", users);
-    });
-
-    // listener for disconnect event, removes user from the list when it happens and emits the new list
-    e.on("disconnect", () => {
-        users = users.filter((item) => item.id !== e.id);
-        io.sockets.emit("users_list", users);
-    });
+  // listener for disconnect event, removes user from the list when it happens and emits the new list
+  e.on("disconnect", () => {
+    users = users.filter((item) => item.id !== e.id);
+    io.sockets.emit("users_list", users);
+  });
 });
 
 // send users list every 15 seconds so the user's list remains updated
 setInterval(() => {
-    io.sockets.emit("users_list", users);
+  io.sockets.emit("users_list", users);
 }, 15000);
 
 server.listen(5000);
