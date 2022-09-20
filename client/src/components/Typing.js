24953b1a3df039776664739a1871a6bf0d269b74
balsa-asanovic <balsaasanovic95@gmail.com>
2022-09-20 23:12:28 +0200

    Increased margin top for typing indicator

--- client/src/components/Typing.js
+++ client/src/components/Typing.js
@@ -1,6 +1,6 @@
 const Typing = () => {
     return (
-        <div className="flex flex-row w-[45px] h-[25px] p-[4px] bg-[#e6e6e6] rounded-2xl space-x-1 items-center">
+        <div className="flex flex-row w-[45px] h-[25px] p-[4px] bg-[#e6e6e6] rounded-2xl space-x-1 items-center mt-1">
             <div className="animate-pulse bg-[#8d8c91] w-[40%] h-[10px] rounded-full"></div>
             <div className="animate-pulse bg-[#8d8c91] w-[40%] h-[10px] rounded-full"></div>
             <div className="animate-pulse bg-[#8d8c91] w-[40%] h-[10px] rounded-full"></div>
